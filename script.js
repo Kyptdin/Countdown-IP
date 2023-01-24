d472b0ca53d94843e7c66c6f52fa28f63d27291f
Kyptdin <isaacestrella12@gmail.com>
2023-01-24 13:32:28 -0500

    Created the input display
    I created the html and css for the input

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+// The diplays
+const daysDisplay = document.querySelector(".count-down-number--days");
+const hoursDisplay = document.querySelector(".count-down-number--hours");
+const minutesDispaly = document.querySelector(".count-down-number--minutes");
+const secondsDisplay = document.querySelector(".count-down-number--seconds");
+
+//Input elements
