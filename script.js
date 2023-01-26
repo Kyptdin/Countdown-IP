e67db04aa46f5811e66f2bf8f34b820acb4da31f
Kyptdin <isaacestrella12@gmail.com>
2023-01-25 20:20:25 -0500

    Query selectors
    I added query selectors for the input and stuff.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 "use strict";
 // The diplays
-const daysDisplay = document.querySelector(".count-down-number--days");
-const hoursDisplay = document.querySelector(".count-down-number--hours");
-const minutesDispaly = document.querySelector(".count-down-number--minutes");
-const secondsDisplay = document.querySelector(".count-down-number--seconds");
+const daysDisplayEl = document.querySelector(".count-down-number--days");
+const hoursDisplayEl = document.querySelector(".count-down-number--hours");
+const minutesDispalyEl = document.querySelector(".count-down-number--minutes");
+const secondsDisplayEl = document.querySelector(".count-down-number--seconds");
 
 //Input elements
+const monthInputEl = document.querySelector(".countdown-form-input--months");
+const dayInputEl = document.querySelector(".countdown-form-input--days");
+const yearInputEl = document.querySelector(".countdown-form-input--years");
+
+//The start countdown buttonm
+const startCountdownBtn = document.querySelector(".btn");
