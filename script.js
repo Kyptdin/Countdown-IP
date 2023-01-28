7f5d0fddf85150dd4ffe98d2b64c3c8335a8f6c4
Kyptdin <isaacestrella12@gmail.com>
2023-01-27 20:22:48 -0500

    Countdown JavaScript
    I created most of the javascript logic. All that's left is to convert miliseconds to days, hours, mintues, and seconds. Then I need to display the time and have a way to stop the time.

--- script.js
+++ script.js
@@ -4,23 +4,49 @@ const daysDisplayEl = document.querySelector(".count-down-number--days");
 const hoursDisplayEl = document.querySelector(".count-down-number--hours");
 const minutesDispalyEl = document.querySelector(".count-down-number--minutes");
 const secondsDisplayEl = document.querySelector(".count-down-number--seconds");
-
-//Input elements
-const monthInputEl = document.querySelector(".countdown-form-input--months");
-const dayInputEl = document.querySelector(".countdown-form-input--days");
-const yearInputEl = document.querySelector(".countdown-form-input--years");
-
+//Input element
+const dateInput = document.querySelector(".date-input");
 //The start countdown buttonm
 const startCountdownBtn = document.querySelector(".btn");
 
-// Count down function
-const countDown = function () {};
+//Determine the min date for the input date
+const determineMinDate = function () {
+  const nowDate = new Date();
+  const year = nowDate.getFullYear();
+  const month = String(nowDate.getMonth() + 1).padStart(2, "0");
+  const day = String(nowDate.getDate() + 1).padStart(2, "0");
 
-startCountdownBtn.addEventListener("click", function () {
-  /* 
-  1. Set the date that we're counting down to
-  2. Create the countdown function
-  */
+  console.log(month, day, year);
+  return `${year}-${month}-${day}`;
+};
 
-  const countDownDate = new Date().getTime(); // inside the date contructor put the values of form
+//Sets the min date based of the determine date
+const setMinDate = function (d) {
+  dateInput.min = `${d}`;
+};
+
+// How to get the goal date from input
+const getGoalDate = function () {
+  const input = String(dateInput.value);
+  const [year, month, day] = input.split("-");
+  return `${year}-${month}-${day}`;
+};
+
+const convertMili = function (t) {
+  let timeLeftClone = t;
+  // Similar to the timer
+};
+
+//Once the user logs in the min date is set
+setMinDate(determineMinDate());
+
+startCountdownBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const goalDate = new Date(getGoalDate());
+  const countDown = setInterval(function () {
+    //Goal date - Date.now() is the miliseconds that have passed since you clicked
+    const timeLeft = goalDate - Date.now();
+    //format the milliseconds to days hours minutes seconds
+    //Get the formatted day, hours, minutes seconds to change the elements
+  }, 1000);
 });
